80116987b71680477b4563c14ed356bf8f218dae
andmade <ashnichdavis@gmail.com>
2016-12-05 20:36:13 -0500

    Add Ink framework files
    Add preliminary routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
+// Homepage
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Public Movie Resource Routes
+Route::get("/movies", "MovieController@index")->name("movies.index");
+Route::get("/movies", "MovieController@show")->name("movies.show");
+
+// Private (Admin Only) Movie Resource Routes
+Route::get("/admin/movies", "MovieController@index")->name("movies.index");
+Route::get("/admin/movies/create", "MovieController@create")->name("movies.create");
+Route::post("/admin/movies/create", "MovieController@store")->name("movies.store");
+Route::get("/admin/movies/show", "MovieController@show")->name("movies.show");
+Route::get("/admin/movies/{movie}/edit", "MovieController@edit")->name("movies.edit");
+Route::put("/admin/movies/{movie}/edit", "MovieController@update")->name("movies.update");
+Route::delete("/admin/movies/{movie}", "MovieController@destory")->name("movies.destroy");
+
+//Queue Resource Routes
+Route::get("/account/queues", "QueueController@index")->name("queues.index");
+Route::get("/account/queues/create", "QueueController@create")->name("queues.create");
+Route::post("/account/queues", "QueueController@store")->name("queues.store");
+Route::get("/account/queues/{queue}", "QueueController@show")->name("queues.show");
+Route::get("/account/queues/{queue}/edit", "QueueController@edit")->name("queues.edit");
+Route::put("/account/queues/{queue}", "QueueController@update")->name("queues.update");
+Route::destroy("/account/queues/{queue}", "QueueController@destroy")->name("queues.destroy");
